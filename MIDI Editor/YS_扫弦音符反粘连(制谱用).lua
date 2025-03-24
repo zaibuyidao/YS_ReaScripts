cfc3e15355a1d6589a54ab9a1f294e8e8ba77787
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2025-03-24 20:07:49 +0800

    Update YS_扫弦音符反粘连(制谱用).lua

--- MIDI Editor/YS_扫弦音符反粘连(制谱用).lua
+++ MIDI Editor/YS_扫弦音符反粘连(制谱用).lua
@@ -1,8 +1,8 @@
 -- @description 扫弦音符反粘连(制谱用)
--- @version 1.0
+-- @version 1.0.1
 -- @author YS
 -- @changelog New Script
--- @provides [main=main,midi_editor] .
+-- @provides [main=main,midi_editor,midi_inlineeditor] .
 
 local editor = reaper.MIDIEditor_GetActive()
 
